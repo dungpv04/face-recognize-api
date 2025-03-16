580f108574da2215a3f05b106e947f72104b4920
Dungpv04 <vandungpham2004@gmail.com>
2025-03-16 18:47:34 +0700

    add cors

--- src/face_recoginze_api/app.py
+++ src/face_recoginze_api/app.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI, Depends
 from face_recoginze_api.routers import faces
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Cho phép React/Vue chạy trên port 3000
+    "https://yourdomain.com",
+    "*",  # Cho phép tất cả nguồn gốc (KHÔNG NÊN dùng trong production)
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Danh sách nguồn gốc được phép
+    allow_credentials=True,  # Cho phép gửi cookies, xác thực
+    allow_methods=["*"],  # Cho phép tất cả phương thức HTTP (GET, POST, PUT, DELETE,...)
+    allow_headers=["*"],  # Cho phép tất cả headers
+)
 
 app.include_router(
     faces.router,
